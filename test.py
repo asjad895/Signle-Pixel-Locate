4c0aa49df469b733afe75911c4be559ff1e227c4
Md Asjad <mdasjad895@gmail.com>
2024-02-06 19:47:05 +0530

    updated scripts

--- test.py
+++ test.py
@@ -1,36 +1,48 @@
 import os
-import logging
 import tensorflow as tf
 import numpy as np
 import pandas as pd
-from single_pixel_locate_train import *
+# from single_pixel_locate_train import *
 import matplotlib.pyplot as plt
 import cv2
-
-# Set up logging
+import logging
+import logging.config
+import sys
+# *******************Data Pipeline*********************************************
+#  tracking for debug
+# Load the logging configuration from the file
+logging.config.fileConfig('logging_config.ini')
+# Get a logger for your module
+logger = logging.getLogger("log/Tesing")
+logger.info("...........Running Testing............")
+# Set up logger
 log_file_path = "log/evaluation_log.txt"
-logging.basicConfig(filename=log_file_path, level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+# logger.basicConfig(filename=log_file_path, level=logger.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 # Load the model
-model_path = "/Trained_model/Face_single_pixel_1.h5"
-loaded_model = tf.keras.models.load_model(model_path)
+try:
+    model_path = "/Trained_model/Face_single_pixel_1.h5"
+    loaded_model = tf.keras.models.load_model(model_path)
+    logger.info("Model loaded successfully.")
+except Exception as e:
+    logger.error("Error loading model:", str(e))
 
 # Evaluate
 try:
     test_loss = loaded_model.evaluate(test_images, test_coor)
     print("Test Loss on unseen data:", test_loss)
-    logging.info("Evaluation completed successfully.")
+    logger.info("Evaluation completed successfully.")
 except Exception as e:
-    logging.error(f"Error during evaluation: {str(e)}")
+    logger.error(f"Error during evaluation: {str(e)}")
 
 # Predict on test images
 try:
     y_pred = loaded_model.predict(test_images)
     # Randomly show results for 5 images
     show_test_result('Data/dataset_lfw_single_coor/test', n_samples=5)
-    logging.info("Random test results displayed.")
+    logger.info("Random test results displayed.")
 except Exception as e:
-    logging.error(f"Error during result display: {str(e)}")
+    logger.error(f"Error during result display: {str(e)}")
 
 # Save results to CSV
 try:
@@ -40,9 +52,9 @@ try:
     image_paths = [os.path.join('Data/Face_data/test', image_file) for image_file in os.listdir('Data/Face_data/test')]
 
     save_df_to_csv(image_paths, y_test_list, y_pred_list, 'test_result.csv')
-    logging.info("Results saved to CSV.")
+    logger.info("Results saved to CSV.")
 except Exception as e:
-    logging.error(f"Error during saving results to CSV: {str(e)}")
+    logger.error(f"Error during saving results to CSV: {str(e)}")
 
 # Save the evaluation log
-logging.shutdown()
+# logger.shutdown()
--- train.py
+++ train.py
@@ -4,10 +4,21 @@ import wandb
 from tensorflow.keras import models, layers, callbacks
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from single_pixel_locate_train import *
-# Set up logging
+# from single_pixel_locate_train import *
+from Pipeline.Data_loading import *
+from Pipeline.Data_preprocessing import *
+import logging.config
+import sys
+
+#  tracking for debug
+# Load the logging configuration from the file
+logging.config.fileConfig('logging_config.ini')
+# Get a logger for your module
+logger = logging.getLogger("log/train")
+logger.info("...........starting training....................")
+# Set up logger
 log_file_path = "log/training_log.txt"
-logging.basicConfig(filename=log_file_path, level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+# logger.basicConfig(filename=log_file_path, level=logger.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 # Initialize WandB for experiment tracking
 wandb.init(project="pixel_localization_experiment", sync_tensorboard=True)
@@ -56,8 +67,28 @@ def create_pixel_localization_model(input_shape=(256, 256, 3)):
     # Compile the model
     model.compile(optimizer=Adam(lr=1e-4), loss='mean_squared_error')
     return model
+try:
+    model = create_pixel_localization_model()
+    logger.info('Creating model... successfully')
+    # learning rate scheduler for dynamic optimization when performance will not improve after 3 epochs
+    lr_scheduler = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, min_lr=1e-6)
+    # early stopping
+    early_stopping = callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
 
-model = create_pixel_localization_model()
+except Exception as e:
+    logger.error('Error creating model:', e)
+
+try:
+    train_images, train_coor = annotate('Data/Face_data/train')
+    test_images, test_coor = annotate('Data/Face_data/test')
+    val_images, val_coor = annotate('Data/Face_data/val')
+    logger.info(f"Data Statistic\n train X {len(train_images)}, y {len(train_coor)}\n test X {len(test_images)}, y {len(test_coor)}\n val X {len(val_images)},y {len(val_coor)}")
+    ColoredOutput.log_message(f"Data Statistic\n train X {len(train_images)}, y {len(train_coor)}\n test X {len(test_images)}, y {len(test_coor)}\n val X {len(val_images)},y {len(val_coor)}","CYAN",True)
+    ColoredOutput.log_message("Data preprocess completed","GREEN",True)
+except Exception as e:
+    logger.error(f"Error during image collection and annotation: {str(e)}")
+    ColoredOutput.log_message(f"Error during image collection and annotation: {str(e)}","RED",True)
+
 
 # Training
 try:
@@ -66,16 +97,21 @@ try:
                         epochs=10, validation_split=0.2, callbacks=[lr_scheduler, early_stopping])
 
     # Log training information
-    logging.info("Training completed successfully.")
+    logger.info("Training completed successfully.")
     wandb.log({"Training Completed": True})
 
 except Exception as e:
     # Log error information
-    logging.error(f"Error during training: {str(e)}")
+    logger.error(f"Error during training: {str(e)}")
     wandb.log({"Training Error": str(e)})
 
 # Save the trained model
-model.save("Trained_model/pixel_localization_model_1.h5")
+try:
+    p="Trained_model/pixel_localization_model_1.h5"
+    model.save(p)
+    logger.info(f"Model saved successfully to path: {p}\n")
+except Exception as e:
+    logger.error(f"Error during model saving: {str(e)}\n")
 
 # Save the training log to WandB
-wandb.save(log_file_path)
+wandb.save('log/my_app.lo')
